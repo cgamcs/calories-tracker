cbe63e22b26f38a43c1a09c16749e81559ec217c
César García <cesar.garciamcs@outlook.com>
2025-09-03 12:10:19 -0600

    creando formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,53 @@
+import { categories } from "../data/category"
 
 function Form() {
   return (
-    <div>Form</div>
+    <form
+      className="space-y-5 bg-white shadow p-10 rounded-2xl"
+    >
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className="font-bold">Categoria:</label>
+        <select
+          id="category"
+          className="border border-slate-200 p-2 rounded-lg w-full bg-white"
+        >
+          {categories.map(category => (
+            <option
+              key={category.id}
+              value={category.id}
+            >
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className="font-bold">Actividad:</label>
+        <input
+          id="activity"
+          type="text"
+          className="border border-slate-200 p-2 rounded-lg w-full bg-white"
+          placeholder="Ej. Comida, Jugo de Naranja, Pesas, Ejercicio, Bicicleta"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">Calorias:</label>
+        <input
+          id="calories"
+          type="number"
+          className="border border-slate-200 p-2 rounded-lg w-full bg-white"
+          placeholder="Ej. 300 o 540"
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="bg-gray-800 hover:bg-gray-900 rounded-lg w-full p-2 font-bold uppercase text-white cursor-pointer"
+        value="Guardar"
+      />
+    </form>
   )
 }
 
