18f18b219d4977e22c96fd589d54839991b4a191
César García <cesar.garciamcs@outlook.com>
2025-09-04 09:27:10 -0600

    agregando información para mostrar categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useReducer } from "react"
 import Form from "./components/Form"
 import { activityReducer, initialState } from "./reducers/activity-reducer"
+import ActivityList from "./components/ActivityList"
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState)
@@ -21,6 +22,12 @@ function App() {
             dispatch={dispatch}
           />
         </div>
+      </section>
+
+      <section className="max-w-4xl mx-auto p-10">
+        <ActivityList
+          activities={state.activities}
+        />
       </section>
     </>
   )
