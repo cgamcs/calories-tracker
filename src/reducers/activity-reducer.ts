96a627950ba3921c8134bf958f0acde637eec44d
César García <cesar.garciamcs@outlook.com>
2025-09-04 14:53:56 -0600

    guardando en localStorage con los cambios del set

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -3,15 +3,21 @@ import type { Activity } from "../types"
 export type ActivityActions =
   { type: 'save-activity', payload: { newActivity: Activity } } |
   { type: 'set-activeId', payload: { id: Activity['id'] } }     |
-  { type: 'delete-activity', payload: { id: Activity['id'] } } 
+  { type: 'delete-activity', payload: { id: Activity['id'] } }  |
+  { type: 'restart-app' } 
 
 export type ActivityState = {
   activities: Activity[]
   activeId: Activity['id']
 }
 
+const localStorageActivities = () : Activity[] => {
+  const activities = localStorage.getItem('activities')
+  return activities ? JSON.parse(activities) : []
+}
+
 export const initialState : ActivityState = {
-  activities: [],
+  activities: localStorageActivities(),
   activeId: ''
 }
 
